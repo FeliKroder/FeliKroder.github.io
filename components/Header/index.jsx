9b688fd7d9dbb01775b1cf47adf80bd41b78172b
Feli Kroder <felicitas.kroder@gmail.com>
2024-02-21 12:20:43 +0100

    Seperate Header from Above the Fold

---
+++ components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { StyledHeader } from "@/design-system/StyledHeader";
+import MainNavigation from "../MainNavigation";
+
+export default function Header() {
+  return (
+    <>
+      <StyledHeader>
+        <p>Feli</p>
+        <MainNavigation></MainNavigation>
+      </StyledHeader>
+    </>
+  );
+}
