1a4ffd09999d60d128396c32d4358eea386d1d76
Feli Kroder <felicitas.kroder@gmail.com>
2024-04-12 13:26:07 +0200

    Implement Mail-connection via EmailJS

--- components/ContactModal/index.jsx
+++ components/ContactModal/index.jsx
@@ -1,3 +1,5 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { StyledContactButton } from "@/design-system/StyledImage";
 import {
   StyledModal,
@@ -31,6 +33,27 @@ export default function ConfirmModal({}) {
     document.body.style.overflow = "auto";
   }
 
+  const form = useRef();
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm("service_yfasj6q", "template_hdb9u34", form.current, {
+        publicKey: "MbCRR0_bvOxVWoTCK",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          event.target.reset();
+          handleHideModal();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <>
       <StyledContactButton
@@ -56,26 +79,30 @@ export default function ConfirmModal({}) {
               Hast du eine Frage, oder möchtest du mich einfach kontaktieren?
               Dann schick mir eine Nachricht.
             </StyledText>
-            <StyledForm action="submit">
+            <StyledForm action="submit" ref={form} onSubmit={sendEmail}>
               <StyledLabel for="name">Name</StyledLabel>
               <StyledInputField
                 type="text"
                 id="name"
+                name="user_name"
                 placeholder="Jane Doe"
               ></StyledInputField>
               <StyledLabel for="email">E-Mail</StyledLabel>
               <StyledInputField
                 type="email"
                 id="email"
+                name="user_email"
                 placeholder="Jane@mail.com"
               ></StyledInputField>
               <StyledTextArea
-                name="textarea"
+                name="message"
                 rows="6"
                 cols="15"
                 placeholder="Hi Feli, ..."
               ></StyledTextArea>
-              <StyledModalButton type="submit">Senden</StyledModalButton>
+              <StyledModalButton type="submit" value="Send">
+                Senden
+              </StyledModalButton>
             </StyledForm>
           </StyledModalContent>
         </StyledModal>
